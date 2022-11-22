ec81f7708311a2d06d8e0f80ec92712c4d155e4b
smldgweb <sales.ldgwebdesign.com>
2022-11-22 17:11:04 -0500

    reworked grid on phones

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,11 +2,57 @@
 const hamburgerIcon = document.getElementById('hamburger');
 const closeIcon = document.getElementById('close-icon');
 const mobileMenuContainer = document.getElementById('mobile-menu-container');
+const productLink = document.getElementById('product-parent');
+const companyLink = document.getElementById('company-parent');
+const connectLink = document.getElementById('connect-parent');
+const parentLinks = document.getElementsByClassName('parent-link');
 const leftMenu = document.querySelector('nav#left-main-menu ul');
 const rightMenu = document.querySelector('nav#right-main-menu ul');
 
+
+
+
+
+//event listeners
 hamburgerIcon.addEventListener('click',openMenu);
 closeIcon.addEventListener('click', closeMenu);
+
+productLink.addEventListener('click', openSubMenu );
+companyLink.addEventListener('click', openSubMenu );
+connectLink.addEventListener('click', openSubMenu );
+
+
+//functions
+function openSubMenu(){
+    if(this === productLink) {
+       const subMenu =  productLink.querySelector('ul.sub-menu');
+       if(subMenu.style.display === "none"){
+            subMenu.style.display = 'block';
+       } else {
+            subMenu.style.display = 'none';
+       }
+    }
+
+    if(this === companyLink) {
+        const subMenu =  companyLink.querySelector('ul.sub-menu');
+        if(subMenu.style.display === "none"){
+             subMenu.style.display = 'block';
+        } else {
+             subMenu.style.display = 'none';
+        }
+     }
+
+     if(this === connectLink) {
+        const subMenu =  connectLink.querySelector('ul.sub-menu');
+        if(subMenu.style.display === "none"){
+             subMenu.style.display = 'block';
+        } else {
+             subMenu.style.display = 'none';
+        }
+     }
+
+
+}
 
 function openMenu(){
     if(hamburgerIcon.className == 'hide') {
